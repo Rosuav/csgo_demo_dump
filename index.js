99d0377367ef6c8dfa0e01526936b2534a411fe1
Chris Angelico <rosuav@gmail.com>
2019-09-15 20:27:33 +1000

    Log round numbers with all weaponry fires

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ function safe(s) {
 	return s.replace(/[:\0-\x1f]/g, ".");
 }
 
+let current_round = 0;
+demo.gameEvents.on("round_start", e => {
+	if (demo.entities.gameRules.isWarmup) current_round = 0;
+	else current_round = demo.entities.gameRules.roundsPlayed + 1;
+});
+
 demo.gameEvents.on("weapon_fire", e => {
 	const player = demo.entities.getByUserId(e.userid);
-	console.log(`weapon_fire:${safe(player.name||e.userid)}:${e.weapon}`);
+	console.log(`weapon_fire:R${current_round}:${safe(player.name||e.userid)}:${e.weapon}`);
 });
 demo.parse(data);

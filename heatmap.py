91b95c2b4524636b838196627f62d0c56890a150
Chris Angelico <rosuav@gmail.com>
2021-05-06 06:28:12 +1000

    Generate multiple heatmaps and an HTML file to show them

--- heatmap.py
+++ heatmap.py
@@ -1,15 +1,5 @@
+from collections import defaultdict
 import png # pip install pypng
-
-data = [
-    [745.2218627929688,808.1373901367188,1],
-    [741.4943237304688,775.325927734375,1],
-    [757.3713989257812,806.7517700195312,1],
-    [-300.1320495605469,1425.9727783203125,1],
-    [-1845.8055419921875,2091.180419921875,1],
-    [-289.85308837890625,1429.4222412109375,1],
-    [-306.8935241699219,1434.9525146484375,1],
-    [-287.78253173828125,1439.9078369140625,1],
-]
 
 # (0,0) on the image should correspond to (MAP_XMIN, MAP_YMIN)
 # If the coordinates need to be inverted, set (eg) XMIN to a high
@@ -75,8 +65,79 @@ def generate_image(img, min, max, rgb_low, rgb_high):
 				out.append(int(lo + (hi - lo) * value))
 		colordata.append(out)
 	return png.from_array(colordata, "RGBA")
-img, peak = build_map(data)
-#for row in img:
-#	print(" ".join(format(x, ".1f") if x >= 1.0 else "---" for x in row))
-#print(peak)
-generate_image(img, 0.875, peak, (0, 64, 0, 255), (240, 255, 240, 255)).save("output.png")
+
+def add_dot_to_image(img, x, y, value, peak):
+	basex, basey = map_to_img(x, y)
+	peak = 0
+	for dx in SPREAD_RANGE:
+		for dy in SPREAD_RANGE:
+			v = img[basey + dy][basex + dx]
+			if dx or dy:
+				altx, alty = img_to_map(basex + dx, basey + dy)
+				dist = (altx - x) ** 2 + (alty - y) ** 2
+				if dist >= DECAY_RADIUS: continue # It's in the corner of the square, too far to be relevant.
+				v += value * ((DECAY_RADIUS - dist) / DECAY_RADIUS) ** 0.5
+				# print("%.3f %3d %d %d %.3f" % (dist, dx**2+ dy**2, dx, dy, ((DECAY_RADIUS - dist) / DECAY_RADIUS) ** 0.5))
+			else: v += value
+			img[basey + dy][basex + dx] = v
+			peak = max(peak, v)
+	return peak
+
+finders = defaultdict(list)
+def finder(key, desc):
+	def deco(func):
+		finders[key].append((func, desc))
+		return func
+	return deco
+
+@finder("smokegrenade_detonate", "Smoke throws")
+def smoke(params):
+	return params[0], params[1], 1
+@finder("flash_hit", "FB detonations") # Scored by opponents blinded
+def flash_pop(params):
+	if params[1] in ("Self", "Team"): return None
+	return params[0], params[2], float(params[4])
+@finder("flash_hit", "FB victims") # Ditto.
+def flash_hit(params):
+	if params[1] in ("Self", "Team"): return None
+	return params[0], params[3], float(params[4])
+
+images = defaultdict(lambda: [[0.0] * IMAGE_WIDTH for _ in range(IMAGE_HEIGHT)])
+img_peaks = defaultdict(int)
+img_descs = defaultdict(str)
+with open("all_data.txt") as f:
+	teams = { }
+	for line in f:
+		if line.startswith("match730_"):
+			# New demo file, identified by file name
+			teams = { }
+			continue
+		key, tick, round, tm, *params = line.strip().split(":")
+		if key == "player":
+			# This one is formatted differently - it doesn't have timing info.
+			# The "tm" field actually gets the player name.
+			teams[tm] = params[1]
+			continue
+		if key == "round_start" and round == "R16":
+			# We don't get the cs_intermission event, so this is our cue that
+			# everyone's teams have switched.
+			for person, t in teams.items():
+				teams[person] = "T" if t == "C" else "C" # assume no spectators
+		for func, desc in finders[key]:
+			who, where, value = func(params) or ('', '', 0)
+			if not value: continue
+			x, y, *_ = where.split(",") # Will have a z coordinate; may also have pitch and yaw.
+			for teamtag in ("", "_" + teams[who]):
+				fn = func.__name__ + "_" + who.split()[0] + teamtag
+				img = images[fn]
+				img_peaks[fn] = add_dot_to_image(img, float(x), float(y), value, img_peaks[fn])
+				img_descs[fn] = desc + " - " + who.split()[0] + " - " + {"_C": "CT", "_T": "T", "": "All"}[teamtag]
+
+with open("template.html") as t, open("heatmap.html", "w") as f:
+	before, after = t.read().split("$$content$$")
+	print(before, file=f)
+	for fn, img in sorted(images.items()):
+		generate_image(img, 0.875, img_peaks[fn], (0, 64, 0, 255), (240, 255, 240, 255)).save(fn + ".png")
+		print(fn + ".png", img_descs[fn])
+		print("<li><label><input type=radio name=picker value=%s> %s</label></li>" % (fn, img_descs[fn]), file=f)
+	print(after, file=f)
